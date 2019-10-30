cfd61ed0788bb0a1352313490d7a2ec236160a0d
Jack Pertschuk <jp954@cornell.edu>
2019-10-30 16:14:38 -0700

    .

--- clients/multisearch.py
+++ clients/multisearch.py
@@ -6,7 +6,7 @@ import requests
 class MutliSearchClient(BaseClient):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.uri = f'http://%s:%s/%s/_msearch' % (
+        self.uri = 'http://%s:%s/%s/_msearch' % (
             self.args.es_host, self.args.es_port, self.args.es_index)
 
     def query(self, query, size):
