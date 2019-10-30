42fec3ef5ab1990aea8a86c0295cc2115a704b23
Jack Pertschuk <jp954@cornell.edu>
2019-10-29 17:29:09 -0700

    .

--- models/distilbert.py
+++ models/distilbert.py
@@ -5,6 +5,7 @@ import numpy as np
 class DBRank(object):
 
     def __init__(self, *args, **kwargs):
+
         self.model_name = 'distilibert'
         self.data_dir = '.model-cache/'
         self.lr = 10e-3
