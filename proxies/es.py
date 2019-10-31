9cbd4882ced1bce704a98142231ce198988f60d1
Jack Pertschuk <jp954@cornell.edu>
2019-10-31 13:56:30 -0700

    .

--- proxies/es.py
+++ proxies/es.py
@@ -1,5 +1,6 @@
 from .base import BaseProxy, RouteHandler
 from typing import Tuple, List, Any
+from ..models.test import TestModel
 
 from aiohttp import web, client
 import aiohttp
@@ -12,16 +13,20 @@ class ESProxy(BaseProxy):
         super().__init__(*args, **kwargs)
         self.host = host
         self.port = port
+        self.model = TestModel(*args)
+        self.requests = dict()
+        self.counter = 0
 
-    @handler.register
-    async def search(self, request: 'web.BaseRequest'):
-        pass
-
-    @handler.register
+    @handler.register(r'/train')
     async def train(self, request: 'web.BaseRequest'):
-        pass
+        data = await request.json()
+        field, query, candidates = self.requests[data['qid']]
+        selected = data['cid'] # candidate that was selected by user
+        labels = [1 if i == selected else 0 for i in range(len(candidates))]
+        self.model.train(query, candidates, labels=labels)
 
-    async def query(self, request: 'web.BaseRequest') -> Tuple[Any, str, List[str], int]:
+    @handler.register(r'/{index}/search')
+    async def search(self, request: 'web.BaseRequest') -> dict:
         request_data = request.json()
         field, query = request.rel_url.query['q'].split(':')
         topk = 10
@@ -30,11 +35,13 @@ class ESProxy(BaseProxy):
             assert resp.status == 200
             data = await resp.json()
             candidates = self.get_candidates(resp, field)
-            return (resp, query, candidates, topk)
+            self.requests[self.counter] = (field, query, candidates)
+            self.counter += 1
+            ranks = self.model.rank(query, candidates)
+            response_dict =  self.reorder(data, ranks)
+            return response_dict
 
-    def reorder(self, response: 'client.ClientResponse', ranks: List[int]) -> dict:
-        assert response.status == 200
-        data = await response.json()
+    def reorder(self, data, ranks: List[int]) -> dict:
         data['hits']['hits']  = [data['hits']['hits'][i] for i in ranks]
         return data
 
