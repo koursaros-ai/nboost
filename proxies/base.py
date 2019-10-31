b51c514d4ba54ce9125c94759ee2ab30873c9c59
colethienes <cole@cole-mbp.local>
2019-10-30 17:20:12 -0700

    .

--- proxies/base.py
+++ proxies/base.py
@@ -13,7 +13,7 @@ STATUS = {
 }
 
 
-class RankProxy:
+class BaseProxy:
     def __init__(self, args):
         super().__init__()
         self.args = args
@@ -21,10 +21,8 @@ class RankProxy:
         self.logger = set_logger(self.__class__.__name__)
         self.model = getattr(models, self.args.model)(self.args)
         self.client = getattr(clients, self.args.client)(self.args)
-
-    @property
-    def status(self):
-        return dict(res='Chillin')
+        self.counter = itertools.count()
+        self.loop = asyncio.get_event_loop()
 
     async def route_handler(self, f):
         @functools.wraps(f)
@@ -40,45 +38,33 @@ class RankProxy:
 
     @route_handler
     async def status(self):
-        return self.status
+        pass
 
     @route_handler
     async def query(self, request):
-        response, candidates, q = await self.client.query(request)
-        ranks = self.model(q, candidates)
-        self.queries[next(counter)] = q, candidates
-        return client.reorder(response, ranks)
+        pass
 
     @route_handler
     async def train(self, request):
-        q, candidates = self.queries[request.query.qid]
-        labels = [0] * len(candidates)
-        labels[request.query.cid] = 1
-        return model(q, candidates, labels=labels)
+        pass
 
-    def create_site(self):
-
-        counter = itertools.count()
-        loop = asyncio.get_event_loop()
-
-    async def main():
+    async def main(self):
         app = web.Application()
         app.add_routes([
-            web.get('/status', status),
-            web.get('/query', query),
-            web.post('/train', train),
+            web.get('/status', self.status),
+            web.get('/query', self.query),
+            web.post('/train', self.train),
         ])
         runner = web.AppRunner(app)
         await runner.setup()
         site = web.TCPSite(runner, self.args.proxy_host, self.args.proxy_port)
         await site.start()
 
-        logger.info('proxy forwarding %s:%d to %s:%d' % (
+    def run(self):
+        self.loop.run_until_complete(self.main())
+        self.logger.info('proxy forwarding %s:%d to %s:%d' % (
             self.args.proxy_host, self.args.proxy_port,
             self.args.server_host, self.args.server_port))
 
-        loop.run_until_complete(main())
-
-    def run(self):
-        self.create_site()
-
+    def stop(self):
+        pass
