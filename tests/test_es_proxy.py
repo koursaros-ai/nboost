322c39d2f352fcc9420d76bc1e3e5607a878d9b0
Jack Pertschuk <jp954@cornell.edu>
2019-10-31 18:13:56 -0700

    .

--- tests/test_es_proxy.py
+++ tests/test_es_proxy.py
@@ -24,13 +24,14 @@ class TestESProxy(unittest.TestCase):
         self.es.indices.create(index='test', ignore=400)
         self.model = TestModel(self.args)
         self.url = 'http://%s:%s/' % (self.args.proxy_host, self.args.proxy_port)
+        self.es_index = 'test'
 
         # id, query, product_title, product_description, median_relevance, relevance_variance
         with open(os.path.join(os.path.dirname(__file__), 'data', 'train.csv'), 'r') as file:
             sample_data = csv.reader(file)
             for row in list(sample_data)[:100]:
                 # print('dumping line %s' % row[2])
-                self.es.index(index="test",
+                self.es.index(index=self.es_index,
                               id=row[0],
                               body={"title": row[2], "description": row[3]})
 
@@ -38,7 +39,8 @@ class TestESProxy(unittest.TestCase):
         client = ESProxy(self.args)
         client.start()
         client.is_ready.wait()
-        res = requests.get(self.url + '_search')
+        res = requests.get(self.url + self.es_index + '/_search?q=description:test')
+        print(res)
         assert res.ok
 
     def test_reorder(self):
