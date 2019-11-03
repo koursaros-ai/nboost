a01490fb8e7f808dd4b9634005b01e150d6a8147
colethienes <cole@cole-mbp.home>
2019-11-03 01:45:50 -0400

    .

--- tests/proxies/es_live.py
+++ tests/proxies/es_live.py
@@ -1,16 +1,14 @@
-import unittest
-from ...cli import set_parser, set_logger
-from ...proxies.rank import ESRankProxy
-from elasticsearch import Elasticsearch
-import requests
-import csv
+
+from ...proxies.rank import ESProxy
+from ..http import HTTPTestCase
 from ...paths import RESOURCES
+from elasticsearch import Elasticsearch
+import csv
 
 
-class TestESProxy(unittest.TestCase):
+class TestESProxy(HTTPTestCase):
 
     def setUp(self):
-        self.logger = set_logger(self.__class__.__name__)
         self.es_index = 'test_index'
         self.es = Elasticsearch()
         self.es.indices.create(index=self.es_index, ignore=400)
@@ -20,22 +18,23 @@ class TestESProxy(unittest.TestCase):
             sample_data = csv.reader(fh)
             self.logger.info('Dumping train.csv...')
             for row in list(sample_data)[:100]:
-                self.es.index(index=self.es_index, id=row[0], body=dict(title=row[2], description=row[3]))
+                self.es.index(
+                    index=self.es_index, id=row[0],
+                    body=dict(title=row[2], description=row[3]))
 
-        parser = set_parser()
-        args = parser.parse_args([
-            '--verbose',
+        self.proxy = self.setUpServer(ESProxy, [
             '--model', 'TestModel',
             '--ext_port', '9200'
         ])
-        self.proxy = ESRankProxy(**vars(args))
-        self.proxy.start()
-        self.proxy.is_ready.wait()
 
     def test_search_and_train(self):
-        res = requests.get(self.proxy.url + '/%s/_search?q=description:test' % self.es_index)
-        self.logger.info('%s:%s' % (res.status_code, res.content))
-        # self.assertEqual(res.json()['msg'], 'server_response')
+        res = self.send(
+            'GET',
+            host=self.proxy.host,
+            port=self.proxy.port,
+            path='/%s/_search' % self.es_index,
+            params={'q': 'description:test'}
+        )
 
     def test_reorder(self):
         pass
