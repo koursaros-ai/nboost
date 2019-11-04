49f67f4c2852bf282b0bec2aa6d84b1528ddd9a4
colethienes <cole.thienes@gmail.com>
2019-11-04 18:20:36 -0500

    .

--- tests/test_es_live.py
+++ tests/test_es_live.py
@@ -1,59 +1,68 @@
 from neural_rerank.proxies import ESProxy
-from http import HTTPTestCase
-from paths import RESOURCES
+from neural_rerank.cli import set_parser
+from tests.paths import RESOURCES
 from elasticsearch import Elasticsearch
 import csv
+import unittest
 import time
 import requests
 
-ES_PORT = 9200
+ES_HOST = '127.0.01'
+ES_PORT = '9200'
 INDEX_SIZE = 1000
+ES_INDEX = 'test_index'
 
 
-class TestESProxy(HTTPTestCase):
+class TestESProxy(unittest.TestCase):
 
     def setUp(self):
         self.es_index = 'test_index'
-        self.proxy = self.setUpServer(ESProxy, [
-            '--model', 'TestModel',
-            '--ext_port', str(ES_PORT),
+        parser = set_parser()
+        self.proxy = ESProxy(**vars(parser.parse_args([
+            '--ext_host', ES_HOST,
+            '--ext_port', ES_PORT,
             '--field', 'description',
-            '--multiplier', '2'
-        ])
+            '--multiplier', '2',
+            '--verbose'
+        ])))
 
         self.es = Elasticsearch()
+        self.proxy.start()
+        self.proxy.is_ready.wait()
 
     def test_es_proxy(self):
 
         try:
-            res = self.send('GET', port=ES_PORT, path='/test_index/_stats')
+            es_res = requests.get(
+                'http://%s:%s/%s/_stats' % (self.proxy.ext_host, self.proxy.ext_port, ES_INDEX)
+            )
         except requests.exceptions.ConnectionError:
-            self.logger.error('ES not available on port %s' % ES_PORT)
-            self.skipTest('')
+            self.skipTest('ES not available on port %s' % ES_PORT)
             raise SystemExit
 
-        if res.json()['_all']['primaries']['docs']['count'] < INDEX_SIZE:
-            self.es.indices.create(index=self.es_index, ignore=400)
+        if es_res.json()['_all']['primaries']['docs']['count'] < INDEX_SIZE:
+            self.es.indices.create(index=ES_INDEX, ignore=400)
 
             # id, query, product_title, product_description, median_relevance, relevance_variance
             with RESOURCES.joinpath('train.csv').open() as fh:
                 sample_data = csv.reader(fh)
-                self.logger.info('Dumping train.csv...')
+                print('Dumping train.csv...')
                 for row in list(sample_data)[:INDEX_SIZE]:
                     self.es.index(
-                        index=self.es_index, id=row[0],
+                        index=ES_INDEX, id=row[0],
                         body=dict(title=row[2], description=row[3]))
 
-        path = '/%s/_search' % self.es_index
-        params = {'q': 'description:light'}
-        res = self.get_from(self.proxy, path=path, params=params)
+        proxy_res = requests.get(
+                'http://%s:%s/%s/_search' % (self.proxy.host, self.proxy.port, ES_INDEX),
+                params={'q': 'description:light'}
+            )
+        self.assertTrue(proxy_res.ok)
 
         self.assertLess(
-            len(res.json()['hits']['hits']),
-            res.json()['hits']['total']
+            len(proxy_res.json()['hits']['hits']),
+            proxy_res.json()['hits']['total']
         )
 
-        status = self.get_from(self.proxy, path='/status')
         # time.sleep(30)
 
     def tearDown(self):
