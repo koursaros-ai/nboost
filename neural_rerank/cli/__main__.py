eb112030fa420953e6ba9c8911af02daa57d2925
Jack Pertschuk <jp954@cornell.edu>
2019-11-05 13:20:46 -0500

    .

--- neural_rerank/cli/__main__.py
+++ neural_rerank/cli/__main__.py
@@ -1,7 +1,7 @@
 from elasticsearch import Elasticsearch
 
-from neural_rerank.clients import ESClient
-from neural_rerank.models import DBERTRank
+from neural_rerank import clients
+from neural_rerank import models
 from neural_rerank.proxies import BaseProxy
 from neural_rerank.cli import set_parser
 
@@ -11,8 +11,8 @@ if __name__ == '__main__':
     parser = set_parser()
     es = Elasticsearch()
     args = parser.parse_args()
-    client = ESClient(**vars(args))
-    model = DBERTRank(**vars(args))
+    client = getattr(clients, args.client)(**vars(args))
+    model = getattr(models, args.model)(**vars(args))
     proxy = BaseProxy(client, model, **vars(args))
     proxy.start()
     proxy.is_ready.wait()
