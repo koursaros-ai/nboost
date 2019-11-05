05b8cfdb63e10ee47f07a2fd6d36523841b059cc
colethienes <cole.thienes@gmail.com>
2019-11-05 15:57:11 -0500

    .

--- neural_rerank/models/base.py
+++ neural_rerank/models/base.py
@@ -1,13 +1,27 @@
 from ..base import Base
+from .handler import ModelHandler
 from typing import List, Union
 from aiohttp import web
 
 
 class BaseModel(Base):
-    def __init__(self, lr: float = 10e-3, data_dir: str = '/.cache', **kwargs):
+    handler = ModelHandler()
+
+    def __init__(self,
+                 lr: float = 10e-3,
+                 data_dir: str = '/.cache',
+                 **kwargs):
         super().__init__(**kwargs)
-        self.lr = lr
-        self.data_dir = data_dir
+        self._lr = lr
+        self._data_dir = data_dir
+
+    @handler.add_state
+    def lr(self):
+        return self._lr
+
+    @handler.add_state
+    def data_dir(self):
+        return self._data_dir
 
     async def train(self,
                     query: str,
---
+++ neural_rerank/proxies/handler.py
@@ -0,0 +1,69 @@
+from ..base import *
+from aiohttp import web_routedef
+from typing import List
+import time
+
+
+class ProxyHandler(BaseHandler):
+    def __init__(self, *handlers: 'ProxyHandler'):
+        super().__init__(*handlers)
+        self.routes = {}
+
+        for handler in handlers:
+            self.routes.update(handler.routes)
+
+    def add_route(self, method: str, path: str):
+        def wrap(f: Callable):
+            self.routes[path] = dict(
+                method=method,
+                path=path,
+                func=f.__name__,
+                lat=float(),
+                reqs=int()
+            )
+
+            async def decorator(proxy, req):
+                proxy.logger.info('RECV: %s' % req)
+                proxy.logger.debug(pfmt(req))
+                self.routes[path]['reqs'] += 1
+                start = time.time()
+                res = await f(proxy, req)
+                self.routes['lat'] = running_avg(
+                    self.routes['lat'],
+                    time.time() - start,
+                    self.routes['reqs']
+                )
+                proxy.logger.info('SEND: %s' % res)
+                proxy.logger.debug(pfmt(req))
+                return res
+
+            return decorator
+
+        return wrap
+
+    def bind_routes(self, proxy) -> List[web_routedef.RouteDef]:
+        return [web.route(
+            self.routes[path]['method'],
+            path,
+            getattr(proxy, self.routes[path]['func'])
+        ) for path in self.routes]
+
+    @staticmethod
+    def plain_ok(body: bytes):
+        return web.Response(body=body, status=200)
+
+    @staticmethod
+    def json_ok(body: dict):
+        return web.json_response(body, status=200)
+
+    @staticmethod
+    def no_content():
+        return web.Response(status=204)
+
+    @staticmethod
+    def redirect(url):
+        raise web.HTTPTemporaryRedirect(url)
+
+    @staticmethod
+    def internal_error(body):
+        return web.json_response(dict(error=str(body), type=type(body).__name__), status=500)
