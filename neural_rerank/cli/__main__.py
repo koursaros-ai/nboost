b76921bee19ca8e40e1f6923d4d734fb06cefd72
Jack Pertschuk <jp954@cornell.edu>
2019-11-06 11:10:37 -0500

    .

--- neural_rerank/cli/__main__.py
+++ neural_rerank/cli/__main__.py
@@ -1,8 +1,9 @@
 from neural_rerank import clients
 from neural_rerank import models
 from neural_rerank.cli import create_proxy
+import sys
 
 if __name__ == '__main__':
-    proxy = create_proxy(client_cls=clients.ESClient, model_cls=models.DBERTRank)
+    proxy = create_proxy(client_cls=clients.ESClient, model_cls=models.DBERTRank, argv=sys.argv[1:])
     proxy.start()
     proxy.is_ready.wait()
