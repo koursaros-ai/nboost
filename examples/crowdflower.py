179dfab51737b959a66869aacf73ab8bb0af0871
Jack Pertschuk <jp954@cornell.edu>
2019-11-06 14:33:17 -0500

    .

---
+++ examples/crowdflower.py
@@ -0,0 +1,20 @@
+from elasticsearch import Elasticsearch
+from tests.paths import RESOURCES
+import csv
+
+INDEX = 'crowdflower'
+
+def main():
+    with RESOURCES.joinpath('train.csv').open() as fh:
+        sample_data = csv.reader(fh)
+        print('Dumping train.csv...')
+        es = Elasticsearch()
+        for row in list(sample_data):
+            es.index(
+                index=INDEX,
+                id=row[0],
+                body=dict(title=row[2], description=row[3])
+            )
+
+if __name__ == '__main__':
+    main()
