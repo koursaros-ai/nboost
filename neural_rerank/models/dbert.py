0296319a892bee63e3b1bd31da8d8e5c7a73f772
Jack Pertschuk <jp954@cornell.edu>
2019-11-07 12:12:48 -0500

    .

--- neural_rerank/models/dbert.py
+++ neural_rerank/models/dbert.py
@@ -75,4 +75,5 @@ class DBERTRank(BaseModel):
         return input_ids, attention_mask
 
     async def save(self):
+        self.logger.info('Saving model')
         self.rerank_model.save_pretrained(self.model_path)
