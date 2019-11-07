a599f724b62f53a5691c9f7b8c295dff87cbb53e
Jack Pertschuk <jp954@cornell.edu>
2019-11-07 17:00:00 -0500

    .

--- examples/es_dump.py
+++ examples/es_dump.py
@@ -30,7 +30,7 @@ MAPPINGS = {
 
 def stream_bodies():
     with open(os.path.join('collection.tsv')) as fh:
-        data = csv.reader(fh)
+        data = csv.reader(fh,delimiter='\t')
         for id, passage in data:
             body = {
                 "_index": INDEX,
