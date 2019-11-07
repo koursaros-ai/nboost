35b3ecd27a1d23ae5c3c3dcc0b99af8dfe94f755
colethienes <cole.thienes@gmail.com>
2019-11-07 18:17:30 -0500

    .

---
+++ neural_rerank/codex/base.py
@@ -0,0 +1,61 @@
+from ..base import StatefulBase
+from typing import Tuple
+from ..base.types import *
+
+
+class BaseCodex(StatefulBase):
+    SEARCH_METHOD = '*'
+    SEARCH_PATH = '/search'
+    TRAIN_METHOD = '*'
+    TRAIN_PATH = '/train'
+    STATUS_METHOD = '*'
+    STATUS_PATH = '/status'
+
+    # for testing
+    NOT_FOUND_METHOD = '*'
+    NOT_FOUND_PATH = '/not_found'
+    ERROR_METHOD = '*'
+    ERROR_PATH = '/error'
+
+    def __init__(self, multiplier: int = 10, field: str = None, **kwargs):
+        super().__init__(**kwargs)
+        self.multiplier = multiplier
+        self.field = field
+
+    def state(self):
+        return dict(multiplier=self.multiplier, field=self.field)
+
+    def magnify(self, req: Request) -> Request:
+        """ Magnify the size of the request by the multiplier """
+        raise NotImplementedError
+
+    def parse(self, req: Request, res: Response) -> Tuple[Query, List[Choice]]:
+        """ Parse out the query and choices """
+        raise NotImplementedError
+
+    def pack(self,
+             req: Request,
+             res: Response,
+             query: Query,
+             choices: List[Choice],
+             ranks: Ranks) -> Response:
+        """
+        Reformat the proxy response according to the reranked candidates
+        mreq is the magnified request.
+        """
+        raise NotImplementedError
+
+    def pluck(self, req: Request) -> Cid:
+        """ Train path retrieves choice id """
+        raise NotImplementedError
+
+    def ack(self, cid: Cid) -> Response:
+        """ acknowledge that train was scheduled """
+        raise NotImplementedError
+
+    def catch(self, e: Exception):
+        """ format the internal error to send to client """
+        raise NotImplementedError
+
+    def pulse(self, state: dict) -> Response:
+        raise NotImplementedError
