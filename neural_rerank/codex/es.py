17dc73e54ae4187f86791269a5ab3a42a7914794
Jack Pertschuk <jp954@cornell.edu>
2019-11-08 16:22:22 -0500

    Merge branch 'master' into cole/fat-updates
    # Conflicts:
    #	neural_rerank/cli/__main__.py
    #	neural_rerank/clients/base.py
    #	neural_rerank/clients/es.py
    #	neural_rerank/model/dbert.py
    #	neural_rerank/proxy/base.py
    #	tests/test_base_proxy.py
    #	tests/test_dbert.py

--- neural_rerank/codex/es.py
+++ neural_rerank/codex/es.py
@@ -53,6 +53,7 @@ class ESCodex(BaseCodex):
             hit['cid'] = choice.id
         body['hits']['hits'] = [body['hits']['hits'][i] for i in ranks[:self.get_topk(req)]]
         res.headers.pop('Content-Length', None)
+        res.headers['Content-Type'] = 'application/json'
         return Response(res.headers, JSON.dumps(body).encode(), 200)
 
     def pluck(self, req):
