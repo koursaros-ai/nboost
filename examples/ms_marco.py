9551d3810210bbfec365d3ad7c9a1a0a95f860ec
Jack Pertschuk <jp954@cornell.edu>
2019-11-08 12:24:54 -0500

    Merge branch 'master' into cole/fat-updates
    # Conflicts:
    #	neural_rerank/cli/__main__.py
    #	neural_rerank/clients/base.py
    #	neural_rerank/clients/es.py
    #	neural_rerank/model/dbert.py
    #	neural_rerank/proxy/base.py
    #	tests/test_base_proxy.py
    #	tests/test_dbert.py

--- examples/ms_marco.py
+++ examples/ms_marco.py
@@ -1,60 +1,80 @@
 import csv, os
 import requests
 from elasticsearch import Elasticsearch
+from collections import defaultdict
 
 INDEX = 'ms_marco'
 DATA_PATH = '.'
 
 es = Elasticsearch()
 
-class defaultdict(dict):
-
-    def __init__(self, cls):
-        super().__init__()
-
 
 def train():
-    qrels = dict()
+    qrels = set()
     train_set = []
+    qid_count = defaultdict(int)
 
     with open(os.path.join(DATA_PATH, 'qrels.train.tsv')) as fh:
         data = csv.reader(fh, delimiter='\t')
         for qid, _, doc_id, _ in data:
-            qrels[doc_id] = qid
-    queries = dict()
+            qrels.add((doc_id, qid))
+            qid_count[qid] += 1
+    # queries = dict()
 
     with open(os.path.join(DATA_PATH, 'queries.train.tsv')) as fh:
         data = csv.reader(fh, delimiter='\t')
         for qid, query in data:
-            queries[qid] = qid
-
-    with open(os.path.join(DATA_PATH, 'collection.tsv')) as fh:
-        data = csv.reader(fh, delimiter='\t')
-        for doc_id, passage in data:
-            if doc_id in qrels:
-                query = queries[qrels[doc_id]]
-                res = es.search(index=INDEX, body={
-                    "query": {
-                        "match": {
-                            "passage": {
-                                "query": query
-                            }
+            res = es.search(index=INDEX, body={
+                "query": {
+                    "match": {
+                        "passage": {
+                            "query": query
                         }
                     }
-                }, filter_path=['hits.hits._*'])
-                candidates = []
-                labels = []
-                for hit in res['hits']['hits']:
-                    if doc_id != hit['_id']:
-                        candidates.append(hit['_source']['passage'])
-                        labels.append(1.0 if doc_id == hit['_id'] else 0.0)
-                candidates.append(passage)
-                labels.append(1.0)
-                requests.request('POST', 'http://localhost:53001/bulk', json={
-                    "query": query,
-                    "candidates": labels,
-                    "labels": labels
-                })
+                }
+            }, filter_path=['hits.hits._*'])
+            qid_hits = defaultdict(lambda: (0,0))
+            for rank, hit in enumerate(res['hits']['hits']):
+                if (qid, hit['_id']) in qrels:
+                    count, rank_sum = qid_hits[qid]
+                    qid_hits[qid] = (count + 1, rank_sum + rank)
+            print("hits: %s, avg rank: %s " % qid_hits[qid], " total: %s" % qid_count[qid])
+
+            # candidates = []
+            # labels = []
+            # for hit in res['hits']['hits']:
+            #     if doc_id != hit['_id']:
+            #         candidates.append(hit['_source']['passage'])
+            #         labels.append(1.0 if doc_id == hit['_id'] else 0.0)
+
+
+# with open(os.path.join(DATA_PATH, 'collection.tsv')) as fh:
+#     data = csv.reader(fh, delimiter='\t')
+#     for doc_id, passage in data:
+#         if doc_id in qrels:
+#             query = queries[qrels[doc_id]]
+#             res = es.search(index=INDEX, body={
+#                 "query": {
+#                     "match": {
+#                         "passage": {
+#                             "query": query
+#                         }
+#                     }
+#                 }
+#             }, filter_path=['hits.hits._*'])
+#             candidates = []
+#             labels = []
+#             for hit in res['hits']['hits']:
+#                 if doc_id != hit['_id']:
+#                     candidates.append(hit['_source']['passage'])
+#                     labels.append(1.0 if doc_id == hit['_id'] else 0.0)
+#             candidates.append(passage)
+#             labels.append(1.0)
+#             requests.request('POST', 'http://localhost:53001/bulk', json={
+#                 "query": query,
+#                 "candidates": labels,
+#                 "labels": labels
+#             })
 
 
 def evaluate():
