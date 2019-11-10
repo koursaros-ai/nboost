e5a0446cb941a5da3b3d98bdf80a0c7128d11fcc
colethienes <cole.thienes@gmail.com>
2019-11-09 21:44:46 -0500

    add

--- tests/test_es_codex.py
+++ tests/test_es_codex.py
@@ -1,39 +1,48 @@
-# from nboost.base.types import *
-# from nboost.codex import ESCodex
-# import json as JSON
-# import requests
-# import unittest
-#
-#
-# class TestESCodex(unittest.TestCase):
-#     def test_es_codex(self):
-#
-#         proxy.start()
-#         server = AioHttpServer(port=6000, ext_port=5900, verbose=True)
-#
-#         async def get_stuff(req):
-#             return Response({}, JSON.dumps(dict(heres='stuff')).encode(), 200)
-#
-#         async def post_stuff(req):
-#             return Response({}, b'I got %s' % req.body, 200)
-#
-#         server.create_app([
-#             ({'/get_stuff': ['GET']}, get_stuff),
-#             ({'/send_stuff': ['POST']}, post_stuff),
-#         ], not_found_handler=lambda x: print(x))
-#
-#         server.start()
-#         server.is_ready.wait()
-#         self.assertTrue(server.is_ready.is_set())
-#
-#         res = requests.get('http://localhost:6000/get_stuff')
-#         print(res.content)
-#         self.assertTrue(res.ok)
-#
-#         res = requests.post('http://localhost:6000/send_stuff',
-#                             data=b'an avocado')
-#         print(res.content)
-#         self.assertTrue(res.ok)
-#
-#         server.stop()
-#         server.join()
+from nboost.paths import RESOURCES
+from nboost.codex import ESCodex
+from nboost.base.types import *
+import json as JSON
+import requests
+import unittest
+
+
+class TestESCodex(unittest.TestCase):
+    def test_es_codex(self):
+        codex = ESCodex(multiplier=5, field='message')
+        query_json = RESOURCES.joinpath('es_query.json').read_bytes()
+        result_json = RESOURCES.joinpath('es_result.json').read_bytes()
+        method = 'GET'
+        path = '/test/_search'
+        headers = {'Content-Length': 250}
+        query_params = {'q': 'message:this is a test', 'size': 20}
+        ranks = Ranks([1, 0, 2])
+        qid = Qid(5)
+        cids = [Cid(1), Cid(2), Cid(3)]
+
+        query_req = Request(method, path, headers, query_params, b'')
+        topk = codex.topk(query_req)
+        self.assertEqual(topk, 20)
+
+        query_mreq = codex.magnify(topk, query_req)
+        self.assertEqual(query_mreq.params['size'], 100)
+
+        json_req = Request(method, path, headers, {}, query_json)
+        json_mreq = codex.magnify(topk, json_req)
+        self.assertEqual(JSON.loads(json_mreq.body)['size'], 100)
+
+        query_mres = Response({}, result_json, 200)
+        query, choices = codex.parse(query_mreq, query_mres)
+        self.assertEqual(query, b'this is a test')
+        self.assertEqual(choices[0], b'trying out Elasticsearch')
+
+        r = codex.pack(topk, query_mres, query, choices, ranks, qid, cids)
+        self.assertNotIn('Content-Length', r.headers)
+
+        train_req = Request('POST', '/train', {}, {'qid': 10, 'cid': 20}, b'')
+        qid, cids = codex.pluck(train_req)
+        self.assertEqual(qid, 10)
+        self.assertEqual(cids, [20])
+
+        err = codex.catch(ValueError('This is an exception'))
+        self.assertEqual(err.status, 500)
+
