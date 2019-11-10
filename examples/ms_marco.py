bba763e149b48abeab5f1a5fb7a7581cca04d5a3
Jack Pertschuk <jp954@cornell.edu>
2019-11-09 20:24:40 -0500

    .

--- examples/ms_marco.py
+++ examples/ms_marco.py
@@ -6,12 +6,15 @@ import time
 
 INDEX = 'ms_marco'
 ES_HOST = '35.238.60.182'
+ES_PORT = 9200
+# ES_HOST = 'localhost'
+# ES_PORT = 53001
 DATA_PATH = '.'
 TOPK = 10
 REQUEST_TIMEOUT = 10000
 
 # es = Elasticsearch(host=ES_HOST)
-es = Elasticsearch(host='localhost',port=53001,timeout=REQUEST_TIMEOUT)
+es = Elasticsearch(host=ES_HOST,port=ES_PORT,timeout=REQUEST_TIMEOUT)
 
 
 def timeit(fn, *args, **kwargs):
@@ -19,23 +22,6 @@ def timeit(fn, *args, **kwargs):
     res = fn(*args, **kwargs)
     print("took %s seconds to run %s" % (time.time() - start, fn.__name__))
     return res
-
-
-def es_latency():
-    es = Elasticsearch(host=ES_HOST)
-    with open(os.path.join(DATA_PATH, 'queries.train.tsv')) as fh:
-        data = csv.reader(fh, delimiter='\t')
-        for qid, query in data:
-            res = timeit(es.search, index=INDEX, body={
-                "size": TOPK,
-                "query": {
-                    "match": {
-                        "passage": {
-                            "query": query
-                        }
-                    }
-                }
-            })
 
 
 def train():
