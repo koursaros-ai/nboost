ad831f9e0505648fe36b1012c84d072e0f85067b
colethienes <cole.thienes@gmail.com>
2019-11-09 23:07:00 -0500

    update db docs/funcs

---
+++ tests/test_hash_db.py
@@ -0,0 +1,19 @@
+from nboost.base.types import *
+from nboost.db import HashDb
+import unittest
+
+
+class TestHashDb(unittest.TestCase):
+    def test_hash_db(self):
+        db = HashDb()
+        q = Query(b'test query')
+        c = Choices([b'choice', b'another choice'])
+        qid, cids = db.save(q, c)
+        self.assertEqual(len(c), len(cids))
+
+        query, choices, labels = db.get(qid, cids)
+        self.assertEqual(q, query)
+        self.assertEqual(c, choices)
+        self.assertEqual(labels, [1.0, 1.0])
+
+
