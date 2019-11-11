8da0c5ba33a2a8b50d6f95d13e3619ff619631f1
Jack Pertschuk <jp954@cornell.edu>
2019-11-11 15:57:14 -0500

    updated README

--- nboost/model/base.py
+++ nboost/model/base.py
@@ -1,6 +1,8 @@
 from ..base import StatefulBase
 from aiohttp import web
 from ..base.types import *
+from .helpers import download_model, extract
+from . import MODEL_PATHS
 import os
 
 
@@ -13,12 +15,25 @@ class BaseModel(StatefulBase):
                  batch_size: int = 4, **kwargs):
         super().__init__()
         self.lr = lr
-        if not os.path.exists(data_dir):
-            os.makedirs(data_dir)
         self.data_dir = data_dir
         self.model_ckpt = model_ckpt
+        self.model_dir = os.path.dirname(self.model_ckpt)
         self.max_seq_len = max_seq_len
         self.batch_size = batch_size
+
+        if not os.path.exists(data_dir):
+            os.makedirs(data_dir)
+        if not os.path.exists(self.model_dir) and self.model_ckpt in MODEL_PATHS:
+            self.model_ckpt = os.path.join(self.data_dir, MODEL_PATHS[self.model_ckpt]['ckpt'])
+            self.model_dir = os.path.dirname(self.model_ckpt)
+            if not os.path.exists(self.model_dir):
+                file = download_model(self.model_ckpt, self.data_dir)
+                extract(file, self.data_dir)
+                os.remove(file)
+                assert os.path.exists(self.model_dir)
+        else:
+            raise FileNotFoundError(self.model_ckpt)
+
 
     def post_start(self):
         """ Executes after the process forks """
---
+++ nboost/model/helpers.py
@@ -0,0 +1,35 @@
+import requests
+import sys
+import os
+import tarfile
+
+from . import MODEL_PATHS
+
+
+def download_model(model, data_dir):
+    link = MODEL_PATHS[model]['url']
+    file_name = os.path.join(data_dir, MODEL_PATHS[model]['url'].split('/')[-1])
+    with open(file_name, "wb") as f:
+        print("Downloading %s" % file_name)
+        response = requests.get(link, stream=True)
+        total_length = response.headers.get('content-length')
+
+        if total_length is None:  # no content length header
+            f.write(response.content)
+        else:
+            dl = 0
+            total_length = int(total_length)
+            for data in response.iter_content(chunk_size=4096):
+                dl += len(data)
+                f.write(data)
+                done = int(50 * dl / total_length)
+                sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50 - done)))
+                sys.stdout.flush()
+    return file_name
+
+
+def extract(archive, data_dir):
+    print("Extracting %s" % archive)
+    tar = tarfile.open(archive)
+    tar.extractall(data_dir)
+    tar.close()
