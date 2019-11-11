3342bc0a36446b5d59a27ed945f9f3f0b54a13e5
Jack Pertschuk <jp954@cornell.edu>
2019-11-11 14:16:35 -0500

    updated README

--- nboost/model/base.py
+++ nboost/model/base.py
@@ -8,13 +8,17 @@ class BaseModel(StatefulBase):
     def __init__(self,
                  lr: float = 10e-3,
                  model_ckpt: str ='./marco_bert',
-                 data_dir: str = '/.cache', **kwargs):
+                 data_dir: str = '/.cache',
+                 max_seq_len: int = 128,
+                 batch_size: int = 4, **kwargs):
         super().__init__()
         self.lr = lr
         if not os.path.exists(data_dir):
             os.makedirs(data_dir)
         self.data_dir = data_dir
         self.model_ckpt = model_ckpt
+        self.max_seq_len = max_seq_len
+        self.batch_size = batch_size
 
     def post_start(self):
         """ Executes after the process forks """
