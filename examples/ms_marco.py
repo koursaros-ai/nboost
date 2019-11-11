dde224f6208e2eebcd774527c2606f6647f05bf5
Jack Pertschuk <jp954@cornell.edu>
2019-11-11 05:03:09 -0500

    updated README

--- examples/ms_marco.py
+++ examples/ms_marco.py
@@ -13,7 +13,6 @@ DATA_PATH = '.'
 TOPK = 10
 REQUEST_TIMEOUT = 10000
 
-# es = Elasticsearch(host=ES_HOST)
 es = Elasticsearch(host=ES_HOST,port=ES_PORT,timeout=REQUEST_TIMEOUT)
 
 
@@ -24,11 +23,8 @@ def timeit(fn, *args, **kwargs):
     return res
 
 
-def train():
+def eval():
     qrels = set()
-    hits = 0
-    total = 0
-    mrr = 0
     qid_count = defaultdict(int)
 
     with open(os.path.join(DATA_PATH, 'qrels.train.tsv')) as fh:
@@ -50,35 +46,15 @@ def train():
                     }
                 }
             }, filter_path=['hits.hits._*'])
-            # query_id = res['qid']
-            qid_hits = defaultdict(lambda: (0, TOPK+1))
-            candidates = []
-            labels = []
-            cids = []
+
+            qid_count = dict()
             for rank, hit in enumerate(res['hits']['hits']):
                 if (qid, hit['_id']) in qrels:
-                    count, min_rank = qid_hits[qid]
-                    qid_hits[qid] = (count + 1, min(min_rank, rank+1))
-                    candidates.append(hit['_source']['passage'])
-                    labels.append(1.0 if doc_id == hit['_id'] else 0.0)
-                    # cids.append(hit['cid'])
-            hits += qid_hits[qid][0]
-            total += qid_count[qid]
-            # if qid_hits[qid][0] > 0:
-            #     timeit(requests.request, 'POST', 'http://localhost:53001/train', json={
-            #         "qid": query_id,
-            #         "cids": cids
-            #     })
-            if qid_hits[qid][1] < TOPK + 1:
-                mrr += (1/qid_hits[qid][1])
-            if total > 0:
-                print("recall @ top %s: %s ." % (TOPK, hits/total), "MRR: %s " % (mrr/total))
-            # print("hits: %s, avg rank: %s " % qid_hits[qid], " total: %s" % qid_count[qid])
+                    print(qid, ' ', rank)
+                    qid_count[qid] = max(qid_count[qid], (1.0 / (float(rank + 1))))
+                    mrr = sum(qid_count.values()) / len(qid_count.keys())
+                    print("MRR: %s " % mrr)
 
-def evaluate():
-    pass
 
 if __name__ == '__main__':
-    # es_latency()
-    train()
-    # evaluate()
+    eval()
