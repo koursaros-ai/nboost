f271164284f9464a7652508df6bbe417d4fa727d
Jack Pertschuk <jp954@cornell.edu>
2019-11-11 13:41:00 -0500

    updated README

--- tests/test_tensorflow_model.py
+++ tests/test_tensorflow_model.py
@@ -9,7 +9,7 @@ import os
 class TestModel(unittest.TestCase):
 
     def setUp(self):
-        if not os.path.exists('bert_marco/bert_config.ckpt'):
+        if not os.path.exists('bert_marco/bert_config.json'):
             raise unittest.SkipTest("Skipping BERT marco test, model binary not found")
         self.model = BertMarcoModel(model_ckpt='bert_marco/bert_model.ckpt')
         self.query = Query('O wherefore art thou'.encode())
