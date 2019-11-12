b8928c286ef07568220e5e741e8c5c33d6f3e238
colethienes <cole.thienes@gmail.com>
2019-11-11 19:46:26 -0500

    Fix/Cli: no args dependency

--- nboost/cli/__init__.py
+++ nboost/cli/__init__.py
@@ -6,7 +6,7 @@ from ..proxy import Proxy
 from typing import List
 
 
-def create_proxy(argv: List[str] = sys.argv[1:]):
+def create_proxy(argv: List[str] = None):
     parser = argparse.ArgumentParser(
         description='%s: is a scalable, search-api-boosting platform for '
                     'developing and deploying SOTA models to improve the '
@@ -16,7 +16,7 @@ def create_proxy(argv: List[str] = sys.argv[1:]):
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--verbose', action='store_true', default=False, help='turn on detailed logging')
     parser.add_argument('--host', type=str, default='127.0.0.1', help='host of the proxy')
-    parser.add_argument('--port', type=int, default=53001, help='port of the proxy')
+    parser.add_argument('--port', type=int, default=8000, help='port of the proxy')
     parser.add_argument('--ext_host', type=str, default='127.0.0.1', help='host of the server')
     parser.add_argument('--ext_port', type=int, default=9200, help='port of the server')
     parser.add_argument('--lr', type=float, default=10e-3, help='learning rate of the model')
