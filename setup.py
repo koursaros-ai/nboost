da56cf0e1f695096717e1982b16686da723fd3f5
Jack Pertschuk <jp954@cornell.edu>
2019-11-14 01:11:14 -0500

    update/documentation

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     ],
     extras_require={
         'torch': ['torch', 'transformers'],
-        'tf': ['tensorflow'],
+        'tensorflow': ['tensorflow'],
     },
     entry_points={'console_scripts': [
         'nboost=nboost.cli.__main__:main',
