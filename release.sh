56028bc87ef9c9426211609825397081c41380fb
colethienes <cole.thienes@gmail.com>
2019-11-21 18:09:41 -0500

    Upgrade/Release: fix deps

--- release.sh
+++ release.sh
@@ -2,5 +2,11 @@ python3 setup.py sdist
 twine upload dist/* -r pypi
 rm -rf dist nboost.egg-info
 echo "Waiting for pypi package to update"
-sleep 15
+
+for i in {001..100};
+do
+    printf "  $i%%\r"
+    sleep 0.6
+done
+
 python3 docker.py
