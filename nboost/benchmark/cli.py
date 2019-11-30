6e739ea76072fa21f9d9ec2cce77dc7145c82686
colethienes <cole.thienes@gmail.com>
2019-11-30 18:23:57 -0500

    Update/Cli: parser construct

--- nboost/benchmark/cli.py
+++ nboost/benchmark/cli.py
@@ -3,21 +3,21 @@ from typing import List
 import termcolor
 import requests
 from nboost.benchmark.benchmarker import Benchmarker
-from nboost.cli import add_default_args
+from nboost.cli import set_parser
 from nboost.proxy import Proxy
 from nboost import DATASET_MAP
 
 
 def main(argv: List[str] = None):
     name = termcolor.colored('NBoost Benchmarker', 'cyan', attrs=['underline'])
-    parser = ArgumentParser(
-        description='This is the %s for nboost, a search-api-boosting platform'
+    parser = set_parser()
+    parser.description = (
+                    'This is the %s for nboost, a search-api-boosting platform'
                     'Use the cli as normal, but instead of hosting a proxy,'
                     'the cli will benchmark speed and efficacy on a specified '
-                    'dataset.' % name,
-        formatter_class=ArgumentDefaultsHelpFormatter)
+                    'dataset.' % name
+    )
 
-    add_default_args(parser)
     parser.add_argument('--rows', type=int, default=-1, help='number of rows for benchmarking')
     parser.add_argument('--connector', type=str, default='ESConnector')
     parser.add_argument('--dataset', type=str, required=True, choices=DATASET_MAP.keys())
