cea05126cf4b5f31638afb7cb64f32733f5533df
Jack Pertschuk <jp954@cornell.edu>
2019-12-14 15:17:29 -0500

    Add / Test case for QA model

---
+++ tests/test_qa_model.py
@@ -0,0 +1,18 @@
+import unittest
+from nboost.models.torch_models.distilbert_qa import TorchDistilBertQAModel
+
+
+class TestTfBertModel(unittest.TestCase):
+
+    def setUp(self):
+        self.model = TorchDistilBertQAModel()
+
+    def test_rank(self):
+        answer, _ = self.model.get_answer('Who bears his memory?', CONTEXT)
+        self.assertEqual(answer, 'His tender heir')
+
+    def tearDown(self) -> None:
+        self.model.close()
+
+
+CONTEXT = 'His tender heir might bear his memory, but thou contracted to thine own bright eyes'
