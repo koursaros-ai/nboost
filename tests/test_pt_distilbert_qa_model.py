4711546658d82ad6a825af7f4ea21006e7c4633f
Jack Pertschuk <jp954@cornell.edu>
2019-12-16 19:13:32 -0500

    Fix/ QA flag

--- tests/test_pt_distilbert_qa_model.py
+++ tests/test_pt_distilbert_qa_model.py
@@ -6,10 +6,14 @@ class TestPtDistilBertQAModel(unittest.TestCase):
 
     def setUp(self):
         self.proxy = Proxy(qa_model_dir='distilbert-base-uncased-distilled-squad',
-                           qa_model='PtDistilBertQAModel', qa=True)
+                           qa_model='PtDistilBertQAModel', max_seq_length=64, qa=True)
 
     def test_rank(self):
         answer, _ = self.proxy.qa_model.get_answer('Who bears his memory?', CONTEXT)
+        self.assertEqual(answer, 'His tender')
+
+    def test_long_rank(self):
+        answer, _ = self.proxy.qa_model.get_answer('Who bears his memory?'*10, CONTEXT)
         self.assertEqual(answer, 'His tender')
 
     def tearDown(self) -> None:
