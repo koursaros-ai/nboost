89a3289cb0ffa44675088bd2c707f48332341af0
colethienes <cole.thienes@gmail.com>
2020-01-05 00:41:04 -0800

    Refactor/Proxy: add runtime configs

--- nboost/proxy.py
+++ nboost/proxy.py
@@ -76,8 +76,8 @@ class Proxy(SocketServer):
         _config = {**CONFIG_MAP[config], **kwargs}
         self.query_path = _config['query_path']
         self.choices_path = _config['choices_path']
-        self.cids_path = self.choices_path + '.[*].' + _config['cids_path']
-        self.cvalues_path = self.choices_path + '.[*].' + _config['cvalues_path']
+        self.cids_path = _config['cids_path']
+        self.cvalues_path = _config['cvalues_path']
         self.topk_path = _config['topk_path']
         self.default_topk = _config['default_topk']
         self.search_path = _config['search_path']
@@ -92,8 +92,8 @@ class Proxy(SocketServer):
                 'query': self.query_path,
                 'topk': self.topk_path,
                 'choices': self.choices_path,
-                'cids': self.cids_path,
-                'cvalues': self.cvalues_path
+                'cids': _config['cids_path'],
+                'cvalues': _config['cvalues_path']
             },
             'description': 'NBoost, for search ranking.'
         }
@@ -105,12 +105,12 @@ class Proxy(SocketServer):
         except ConnectionRefusedError:
             raise UpstreamConnectionError
 
-    def get_topk(self, request: dict) -> int:
-        topks = get_jsonpath(request, self.topk_path)
-        return int(topks[0]) if topks else self.default_topk
+    def get_topk(self, request: dict, topk_path: str, default_topk: str) -> int:
+        topks = get_jsonpath(request, topk_path)
+        return int(topks[0]) if topks else default_topk
 
-    def get_query(self, request: dict) -> str:
-        queries = get_jsonpath(request, self.query_path)
+    def get_query(self, request: dict, query_path: str) -> str:
+        queries = get_jsonpath(request, query_path)
         query = self.delim.join(queries)
 
         # check for errors
@@ -119,8 +119,8 @@ class Proxy(SocketServer):
 
         return query
 
-    def get_cids(self, response: dict) -> list:
-        cids = get_jsonpath(response, self.cids_path)
+    def get_cids(self, response: dict, cids_path: str) -> list:
+        cids = get_jsonpath(response, cids_path)
         return flatten(cids)
 
     def get_rerank_cids(self, request: dict) -> list:
@@ -131,11 +131,11 @@ class Proxy(SocketServer):
         qa_cids = get_jsonpath(request, QA_CIDS)
         return qa_cids[0] if qa_cids else None
 
-    def get_cvalues(self, response: dict) -> list:
-        return get_jsonpath(response, self.cvalues_path)
+    def get_cvalues(self, response: dict, cvalues_path: str) -> list:
+        return get_jsonpath(response, cvalues_path)
 
-    def get_choices(self, response: dict) -> list:
-        choices = get_jsonpath(response, self.choices_path)
+    def get_choices(self, response: dict, choices_path: str) -> list:
+        choices = get_jsonpath(response, choices_path)
 
         if not isinstance(choices, list):
             raise InvalidChoices('choices were not a list')
@@ -163,9 +163,17 @@ class Proxy(SocketServer):
 
             hooks.on_client_request(client_socket, request, buffer, self.search_path)
             self.logger.debug(prefix + 'search request.')
+            runtime_configs = request['body'].pop('nboost', {})
 
-            topk = self.get_topk(request)
-            query = self.get_query(request)
+            query_path = runtime_configs.get('query_path', self.query_path)
+            choices_path = runtime_configs.get('choices_path', self.choices_path)
+            cids_path = choices_path + '.[*].' + runtime_configs.get('cids_path', self.cids_path)
+            cvalues_path = choices_path + '.[*].' + runtime_configs.get('cvalues_path', self.cvalues_path)
+            topk_path = runtime_configs.get('topk_path', self.topk_path)
+            default_topk = runtime_configs.get('default_topk', self.default_topk)
+
+            topk = self.get_topk(request, topk_path, default_topk)
+            query = self.get_query(request, query_path)
 
             if self.rerank:
                 self.update_averages(topk=topk)
@@ -173,11 +181,12 @@ class Proxy(SocketServer):
 
             hooks.on_server_request(server_socket, request)
             hooks.on_server_response(server_socket, response)
+            response['body']['nboost'] = {}
 
             if self.rerank:
-                choices = self.get_choices(response)
-                cids = self.get_cids(response)
-                cvalues = self.get_cvalues(response)
+                choices = self.get_choices(response, choices_path)
+                cids = self.get_cids(response, cids_path)
+                cvalues = self.get_cvalues(response, cvalues_path)
                 rerank_cids = self.get_rerank_cids(request)
 
                 if rerank_cids:
@@ -190,33 +199,30 @@ class Proxy(SocketServer):
                 self.update_averages(rerank_time=total_time)
                 reranked_choices = [choices[rank] for rank in ranks]
                 set_jsonpath(response, self.choices_path, reranked_choices)
-                cids = self.get_cids(response)
+                cids = self.get_cids(response, cids_path)
 
                 if rerank_cids:
                     model_mrr = calculate_mrr(rerank_cids, cids)
                     self.update_averages(model_mrr=model_mrr)
 
             if self.qa:
-                cids = self.get_cids(response)
-                cvalues = self.get_cvalues(response)
+                cids = self.get_cids(response, cids_path)
+                cvalues = self.get_cvalues(response, cvalues_path)
                 qa_cids = self.get_qa_cids(request)
 
-                if cids and cvalues:
+                if cvalues:
                     start_time = time.perf_counter()
-                    answer, start_pos, end_pos, score = self.qa_model.get_answer(query, cvalues[0])
+                    answer, start_pos, stop_pos, score = self.qa_model.get_answer(query, cvalues[0])
                     total_time = time.perf_counter() - start_time
                     self.update_averages(qa_time=total_time)
 
                     if score > self.qa_threshold:
-                        response['body']['nboost']['qa'] = {
-                            'answer': answer,
-                            'start_pos': start_pos,
-                            'end_pos': end_pos,
-                            'score': score
-                        }
-                        if qa_cids and cids[0] in qa_cids.keys():
+                        response['body']['nboost']['answerStartPos'] = start_pos
+                        response['body']['nboost']['answerStopPos'] = stop_pos
+
+                        if qa_cids and cids and cids[0] in qa_cids.keys():
                             qa_start_pos, qa_end_pos = qa_cids[cids[0]]
-                            overlap = calculate_overlap(qa_start_pos,qa_end_pos, start_pos, end_pos)
+                            overlap = calculate_overlap(qa_start_pos,qa_end_pos, start_pos, stop_pos)
                             self.update_averages(qa_overlap=overlap)
 
             hooks.on_client_response(request, response, client_socket)
