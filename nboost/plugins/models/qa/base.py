5193acc2747dbe1a190b7851fe848f44ee76bf8e
Jack Pertschuk <jp954@cornell.edu>
2020-02-28 00:41:45 -0800

    .

--- nboost/plugins/models/qa/base.py
+++ nboost/plugins/models/qa/base.py
@@ -20,9 +20,11 @@ class QAModelPlugin(ModelPlugin):
 
             db_row.qa_time = time.perf_counter() - start_time
 
-            response.set_path('json.nboost.answer_text', answer)
-            response.set_path('json.nboost.answer_start_pos', start_pos)
-            response.set_path('json.nboost.answer_stop_pos', stop_pos)
+            print(answer, start_pos, stop_pos, score)
+            if score > response.request.qa_threshold:
+                response.set_path('json.nboost.answer_text', answer)
+                response.set_path('json.nboost.answer_start_pos', start_pos)
+                response.set_path('json.nboost.answer_stop_pos', stop_pos)
 
     def get_answer(self, query: str, cvalue: str) -> Tuple[str, int, int, float]:
         """Return answer, start_pos, end_pos, score"""
