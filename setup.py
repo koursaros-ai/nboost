fa394225226579df19428896f5903f6e9a4fb31b
Jack Pertschuk <jp954@cornell.edu>
2020-03-07 13:56:16 -0800

    Bump BERT to TF 2.1

--- setup.py
+++ setup.py
@@ -35,8 +35,8 @@ setup(
     ],
     extras_require={
         'pt': ['torch', 'transformers==2.2.1'],
-        'tf': ['tensorflow>=1.14', 'sentencepiece', 'tensorflow_text', 'tensorflow_hub'],
-        'all': ['torch', 'tensorflow>=1.14', 'transformers==2.2.1'],
+        'tf': ['tensorflow>=2.1.0', 'sentencepiece'],
+        'all': ['torch', 'tensorflow>=2.1.0', 'transformers==2.2.1'],
     },
     entry_points={'console_scripts': [
         'nboost=nboost.__main__:main',
