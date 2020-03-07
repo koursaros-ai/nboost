97aada3a634106606d9cd422cf9ad7900f25b988
Jack Pertschuk <jp954@cornell.edu>
2020-03-06 17:21:05 -0800

    .

--- nboost/plugins/models/rerank/tf/bert/modeling.py
+++ nboost/plugins/models/rerank/tf/bert/modeling.py
@@ -362,7 +362,7 @@ def dropout(input_tensor, dropout_prob):
 
 def layer_norm(input_tensor, name=None):
   """Run layer normalization on the last dimension of the tensor."""
-  return tf.compat.v1.contrib.layers.layer_norm(
+  return tf.contrib.layers.layer_norm(
       inputs=input_tensor, begin_norm_axis=-1, begin_params_axis=-1, scope=name)
 
 
