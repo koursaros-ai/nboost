c4b11c1d24a457ed6b54a729f15f01cea4863873
eltu <helton.alponti@gmail.com>
2020-05-18 10:50:47 -0300

    e for integration with solr

--- tests/integration/test_solr_indexer.py
+++ tests/integration/test_solr_indexer.py
@@ -3,13 +3,13 @@ import subprocess
 import unittest
 import requests
 import time
-
+import sys
 
 class TestSolrIndexer(unittest.TestCase):
     def test_travel_tutorial(self):
         subprocess.call('docker pull solr:8.5.1', shell=True)
-        subprocess.call('docker run -d -p 8983:8983 -t solr solr-precreate travel',
-                        shell=True)
+        subprocess.call("docker rm -f solr_integration", shell=True)
+        subprocess.call('docker run -d -p 8983:8983 --name solr_integration -t solr solr-precreate travel', shell=True)
 
         #TODO: check if the core is ready by the api
         time.sleep(20.0)
@@ -30,4 +30,3 @@ class TestSolrIndexer(unittest.TestCase):
 
         proxy_res = requests.get('http://localhost:8983/solr/travel/select', params=params)
         self.assertTrue(proxy_res.ok)
-        exit(0)
