3fa360e95b73249213b9c4d30170577a6e858f02
eltu <helton.alponti@gmail.com>
2020-05-18 11:31:27 -0300

    fix integration test for es

--- tests/integration/test_es_indexer.py
+++ tests/integration/test_es_indexer.py
@@ -2,14 +2,19 @@ from nboost.indexers.cli import main
 import subprocess
 import unittest
 import requests
-
+import time
 
 class TestESIndexer(unittest.TestCase):
     def test_travel_tutorial(self):
         subprocess.call('docker pull elasticsearch:7.4.2', shell=True)
-        subprocess.call('docker run -d -p 9200:9200 -p 9300:9300 '
+        subprocess.call("docker rm -f es_integration", shell=True)
+
+        subprocess.call('docker run -d -p 9200:9200 -p 9300:9300 --name es_integration '
                         '-e "discovery.type=single-node" elasticsearch:7.4.2',
                         shell=True)
+
+        #TODO: check if the core is ready by the api
+        time.sleep(20.0)
 
         # dump es index
         main([
@@ -26,4 +31,5 @@ class TestESIndexer(unittest.TestCase):
 
         proxy_res = requests.get('http://localhost:9200/travel/_search', params=params)
         self.assertTrue(proxy_res.ok)
-        exit(0)
+        
+        subprocess.call("docker rm -f es_integration", shell=True)
